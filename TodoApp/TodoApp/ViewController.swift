95ec5fded324a50f2bfef5edb94a14a55c004988
Pakornpat Sinjiranon <skyfox.ku@gmail.com>
2022-06-27 20:32:36 +0700

    Add table view delegate

--- TodoApp/TodoApp/ViewController.swift
+++ TodoApp/TodoApp/ViewController.swift
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class ViewController: UIViewController, UITableViewDataSource {
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
 
     var todo = Todo()
 
@@ -21,6 +21,10 @@ class ViewController: UIViewController, UITableViewDataSource {
         cell.textLabel?.text = item.title
         cell.accessoryType = item.isDone ? .checkmark : .none
         return cell
+    }
+
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
     }
 
     override func viewDidLoad() {
