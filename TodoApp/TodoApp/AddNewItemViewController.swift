4a8b6041ee18ce2a61a2899700eec5ba2a5311ac
Pakornpat Sinjiranon <skyfox.ku@gmail.com>
2022-06-27 22:18:35 +0700

    Add new view controller for add new item

---
+++ TodoApp/TodoApp/AddNewItemViewController.swift
@@ -0,0 +1,34 @@
+//
+//  AddNewItemViewController.swift
+//  TodoApp
+//
+//  Created by Pakornpat Sinjiranon on 27/6/22.
+//
+
+import UIKit
+
+class AddNewItemViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    @IBAction func doneButtonDidTap(_ sender: UIBarButtonItem) {
+        print("done")
+    }
+
+    @IBAction func cancelButtonDidTap(_ sender: UIBarButtonItem) {
+        print("cancel")
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
