e7f35df5010655dd9abecf2a30dd52bc75cec10e
Pakornpat Sinjiranon <skyfox.ku@gmail.com>
2022-06-28 11:20:01 +0700

    Binding for add new item

--- TodoApp/TodoApp/AddNewItemViewController.swift
+++ TodoApp/TodoApp/AddNewItemViewController.swift
@@ -16,12 +16,23 @@ class AddNewItemViewController: UIViewController {
 
     weak var delegate: AddNewItemViewControllerDelegate?
 
+    @IBOutlet weak var titleTextField: UITextField!
+    @IBOutlet weak var isDoneSwitch: UISwitch!
+
     override func viewDidLoad() {
         super.viewDidLoad()
     }
+
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        titleTextField.becomeFirstResponder()
+    }
     
     @IBAction func doneButtonDidTap(_ sender: UIBarButtonItem) {
-        delegate?.addNewItemViewController(controller: self, didAdd: TodoItem(title: "Test"))
+        if let title = titleTextField.text, !title.isEmpty {
+            let todoItem = TodoItem(title: title, isDone: isDoneSwitch.isOn)
+            delegate?.addNewItemViewController(controller: self, didAdd: todoItem)
+        }
     }
 
     @IBAction func cancelButtonDidTap(_ sender: UIBarButtonItem) {
