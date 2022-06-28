c4b3d45d38f8bf57e207543a870779fc3268cb43
Pakornpat Sinjiranon <skyfox.ku@gmail.com>
2022-06-28 13:05:59 +0700

    Edit todo item

--- TodoApp/TodoApp/AddNewItemViewController.swift
+++ TodoApp/TodoApp/AddNewItemViewController.swift
@@ -9,11 +9,13 @@ import UIKit
 
 protocol AddNewItemViewControllerDelegate: AnyObject {
     func addNewItemViewController(controller: AddNewItemViewController, didAdd item: TodoItem)
+    func addNewItemViewController(controller: AddNewItemViewController, didEdit item: TodoItem)
     func addNewItemViewControllerDidCancel(controller: AddNewItemViewController)
 }
 
 class AddNewItemViewController: UIViewController {
 
+    var todoItem: TodoItem?
     weak var delegate: AddNewItemViewControllerDelegate?
 
     @IBOutlet weak var titleTextField: UITextField!
@@ -21,6 +23,13 @@ class AddNewItemViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        if let todoItem = todoItem {
+            title = "Edit item"
+            titleTextField.text = todoItem.title
+            isDoneSwitch.setOn(todoItem.isDone, animated: true)
+        } else {
+            title = "Add new item"
+        }
     }
 
     override func viewDidAppear(_ animated: Bool) {
@@ -29,7 +38,14 @@ class AddNewItemViewController: UIViewController {
     }
     
     @IBAction func doneButtonDidTap(_ sender: UIBarButtonItem) {
-        if let title = titleTextField.text, !title.isEmpty {
+        guard let title = titleTextField.text, !title.isEmpty else {
+            return
+        }
+        if let todoItem = todoItem {
+            todoItem.title = title
+            todoItem.isDone = isDoneSwitch.isOn
+            delegate?.addNewItemViewController(controller: self, didEdit: todoItem)
+        } else {
             let todoItem = TodoItem(title: title, isDone: isDoneSwitch.isOn)
             delegate?.addNewItemViewController(controller: self, didAdd: todoItem)
         }
--- TodoApp/TodoApp/ViewController.swift
+++ TodoApp/TodoApp/ViewController.swift
@@ -27,12 +27,27 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
 
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         tableView.deselectRow(at: indexPath, animated: true)
+        performSegue(withIdentifier: "openEditItemSegue", sender: todo.item(at: indexPath.row))
+    }
+
+    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            todo.remove(at: indexPath.row)
+            tableView.deleteRows(at: [indexPath], with: .automatic)
+        }
     }
 
     func addNewItemViewController(controller: AddNewItemViewController, didAdd item: TodoItem) {
         todo.add(item: item)
         if let index = todo.index(of: item) {
             tableView?.insertRows(at: [IndexPath(row: index, section: 0)], with: .automatic)
+        }
+        controller.dismiss(animated: true, completion: nil)
+    }
+
+    func addNewItemViewController(controller: AddNewItemViewController, didEdit item: TodoItem) {
+        if let index = todo.index(of: item) {
+            tableView?.reloadRows(at: [IndexPath(row: index, section: 0)], with: .automatic)
         }
         controller.dismiss(animated: true, completion: nil)
     }
@@ -52,6 +67,12 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
         if segue.identifier == "openAddItemSegue" {
             if let nav = segue.destination as? UINavigationController,
                 let controller = nav.topViewController as? AddNewItemViewController {
+                controller.delegate = self
+            }
+        } else if segue.identifier == "openEditItemSegue" {
+            if let nav = segue.destination as? UINavigationController,
+                let controller = nav.topViewController as? AddNewItemViewController {
+                controller.todoItem = sender as? TodoItem
                 controller.delegate = self
             }
         }
